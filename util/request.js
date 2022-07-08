17a457db5685edc8c58be2dbcf7fdcc095ba7cbc
kcn3388 <kcn3388@126.com>
2022-07-08 16:37:40 +0800

    personal 20220708

--- util/request.js
+++ util/request.js
@@ -55,7 +55,7 @@ const createRequest = (method, url, data = {}, options) => {
       headers['X-Real-IP'] = ip
       headers['X-Forwarded-For'] = ip
     }
-    // headers['X-Real-IP'] = '118.88.88.88'
+    // headers['X-Real-IP'] = '159.75.78.23'
     if (typeof options.cookie === 'object') {
       if (!options.cookie.MUSIC_U) {
         // 游客
